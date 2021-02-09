74fbc62f312ec480ee15e34a21215e6b345f8788
Aidan Do <giahuydo99@gmail.com>
2021-02-09 17:40:28 +1030

    feat: can now save user

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,4 +1,20 @@
 import { Request, Response, NextFunction } from "express";
-export function postRegister(req: Request, res: Response, next: NextFunction) {
-
+import { User } from "../models/User";
+export async function postRegister(req: Request, res: Response) {
+  if (req.get("Content-Type") !== "application/json")
+    return res.sendStatus(415);
+  try {
+    const user = new User({
+      email: req.body.email,
+      username: req.body.username,
+      password: req.body.password
+    });
+    const userDocument = await user.save();
+    const {__v, password, ...result} = userDocument.toObject();
+    return res.status(200).send(result);
+  } catch(error) {
+    return res.status(400).send({
+      errors: error.errors
+    });
+  }
 }
--- test/controllers/user.test.ts
+++ test/controllers/user.test.ts
@@ -1,14 +1,101 @@
 import request from "supertest";
 import app from "../../src/app";
+import _ from "lodash";
+import { User } from "../../src/models/User";
 
 describe("User routes", () => {
   describe("/register", () => {
-    test("Can register", (done) => {
+    let user: User;
+
+    beforeEach(() => {
+      user = {
+        username: "testUser18",
+        password: "testUser18",
+        email: "testUser18@gmail.com"
+      }
+    })
+
+    test("Should 415 for non json content types", (done) => {
       request(app)
         .post("/register")
-        .send({ identifer: "testUser18", password: "testUser18" })
-        .expect(200, done);
+        .set("Content-Type", "text/plain")
+        .send('{ identifer: "testUser18", password: "testUser18" }')
+        .expect(415, done);
     });
-  })
 
+    describe("after sending a user", () => {
+      let userRequest: request.Test;
+      beforeEach(() => {
+        userRequest = request(app)
+          .post("/register")
+          .set("Content-Type", "application/json")
+      })
+
+      test("Should 200 for valid input", async () => {
+        const res = await userRequest
+          .send(user)
+          .expect(200);
+
+        const {_id, ...result} = res.body;
+        expect(_.isMatch(user, result)).toBe(true);
+        const users = await User.find({ _id });
+        expect(users.length).toBe(1);
+        expect(users[0].id).toBe(_id);
+      });
+
+      test("should provide a message for empty username", (done) => {
+        user.username = "";
+
+        userRequest
+          .send(user)
+          .expect(400, (err, res) => {
+            if (err) done(err);
+            expect(res.body.errors.username.kind).toBe('required');
+            done();
+          })
+      })
+
+      test("should provide a message for empty email", (done) => {
+        user.email = "";
+
+        userRequest
+          .send(user)
+          .expect(400, (err, res) => {
+            if (err) done(err);
+            expect(res.body.errors.email.kind).toBe('required');
+            done();
+          })
+      })
+
+      test("should provide a message for non-compliant RFC2822 email", async () => {
+        user.email = "w@w"
+
+        const res = await userRequest
+          .send(user)
+          .expect(400)
+
+        expect(res.body.errors.email.kind).toBe('regexp');
+      })
+
+      test("should provide a message for not providing username", async () => {
+        delete user.username;
+        const res = await userRequest
+          .send(user)
+          .expect(400)
+        
+        expect(res.body.errors.username.kind).toBe('required');
+      })
+
+      test("should provide a message for not providing password", async () => {
+        delete user.password;
+
+        const res = await userRequest
+          .send(user)
+          .expect(400)
+
+        expect(res.body.errors.password.kind).toBe('required');
+      });
+    })
+
+  });
 });
